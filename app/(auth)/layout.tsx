5bfacc5d3a8923b63170ab370c4740278727ff45
siroojuddin <rojudin123@gmail.com>
2026-05-07 15:53:01 +0700

    feat: redirect authenticated users to chat page with a delay in auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,22 @@
 "use client"
 
 import { useHasSession } from "@/hooks/use-has-session"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
-export default function Layout({children}:{children:React.ReactNode}){
+export default function Layout({ children }: { children: React.ReactNode }) {
 
-    useHasSession()
+    const { hasSession } = useHasSession()
+    const router = useRouter()
+    useEffect(() => {
+        if (hasSession) {
+            const interval = setInterval(() => {
+                router.push(`/chat`);
+            }, 3000);
+
+            return () => clearInterval(interval);
+        }
+    }, [hasSession, router]);
 
     return (
         <div className="h-screen w-full flex items-center justify-center">
